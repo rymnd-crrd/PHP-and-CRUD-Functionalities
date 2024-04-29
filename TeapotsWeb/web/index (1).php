cd5af0e4b2405b2e11dc77acd645921967a779a1
rymnd-crrd <161231297+rymnd-crrd@users.noreply.github.com>
2024-04-30 03:22:50 +0800

    Add files via upload

---
+++ TeapotsWeb/web/index (1).php
@@ -0,0 +1,22 @@
+   <html lang="en">
+   <head>
+    <body>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> 
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Font+Name&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap">
+
+
+  <div class="header"> 
+   <h1>WELCOME TO</h1>
+   <h2>My Teapots</h2>
+   <h3>Milktea & Coffee</h3>
+    
+   <a href="home.html" class="button">VIEW MORE</a> 
+    <div class="overlay"></div>
+   <script src="try.js"></script>
+  </div>   
+ </body>
+</html>
